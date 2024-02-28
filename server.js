a81532123ac1cc49538ef1e6216b6fa4afbbfa3a
kelvin <kelvin80121@hotmail.com>
2024-02-28 21:28:35 +0800

    update

---
+++ server.js
@@ -0,0 +1,88 @@
+const http = require("http");
+const Room = require("./models/room");
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
+
+dotenv.config({path: "./.env"})
+console.log(process.env.PORT)
+
+// 連接資料庫
+mongoose.connect('mongodb+srv://kelvin80121:iEK0Zh2uWzgUnNLK@cluster0.x7cwh1p.mongodb.net/')
+    .then(()=>{
+        console.log('資料庫連線成功')
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
+const requestListener = async (req,res)=>{
+    let body = "";
+    req.on('data',chunk=>{
+        body+=chunk;
+    })
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
+    if(req.url=="/rooms" && req.method=="GET"){
+        const rooms = await Room.find();
+        res.writeHead(200,headers);
+        res.write(JSON.stringify({
+            "status":"success",
+            rooms
+        }))
+        res.end()
+    }else if(req.url="/rooms" && req.method=="POST"){
+        req.on('end', async()=>{
+            try{
+                const data = JSON.parse(body);
+                const newRoom = await Room.create(
+                    {
+                        name: data.name,
+                        price: data.price,
+                        rating: data.rating
+                    }
+                )
+                res.writeHead(200,headers);
+                res.write(JSON.stringify({
+                    "status":"success",
+                    rooms: newRoom
+                }))
+                res.end()
+            }catch(error){
+                res.writeHead(400,headers);
+                res.write(JSON.stringify({
+                    "status": "false",
+                    "message":"欄位沒有正確，或沒有此 ID",
+                    "error": error
+                }))
+                res.end()
+            }
+        })
+    }else if(req.url="/rooms" && req.method=="DELETE"){
+        await Room.deleteMany({});
+        res.writeHead(200,headers);
+        res.write(JSON.stringify({
+            "status":"success",
+            rooms:[]
+        }))
+        res.end();
+    }
+    else if(req.method == "OPTIONS"){
+        res.writeHead(200,headers);
+        res.end();
+    }else{
+        res.writeHead(404,headers);
+        res.write(JSON.stringify({
+            "status": "false",
+            "message": "無此網站路由"
+        }));
+        res.end();
+    }
+}
+
+const server = http.createServer(requestListener);
+server.listen(process.env.PORT);
